b80c95406351a15f1f2a79204ee086f8be28466e
Zachary King <zach_a_king@outlook.com>
2021-09-02 14:38:55 -0400

    feat(sort): :sparkles:  Class structure for different data types
    Now have the ability to sort arrays of numbers, strings, and linked lists

--- build/index.js
+++ build/index.js
@@ -1,5 +1,18 @@
 "use strict";
-console.log("Hi There");
-var logSomething = function () {
-    console.log("something");
-};
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
+var numberCollection = new NumbersCollection_1.NumbersCollection([1000, 3, -5, 0]);
+numberCollection.sort();
+console.log(numberCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection("aFGksles");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-console.log("Hi There");
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-const logSomething = () => {
-  console.log("something");
-};
+const numberCollection = new NumbersCollection([1000, 3, -5, 0]);
+numberCollection.sort();
+console.log(numberCollection.data);
+
+const charactersCollection = new CharactersCollection("aFGksles");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
